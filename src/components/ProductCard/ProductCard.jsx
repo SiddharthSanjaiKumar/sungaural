abd38c60a85a4d59893339d96acdc048634b9ee8
Siddharth Sanjaikumar <siddharth.vsk2000@gmail.com>
2025-11-08 15:40:38 +0530

    Final Push.

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import './ProductCard.css';
-// import serumImage from '../../assets/images/serum.png';
+import facewashImage from '../../assets/Images/facewash.png';
+import facewash1Image from '../../assets/Images/facewash1.png';
+import serumImage from '../../assets/Images/serum.png';
+import serum1Image from '../../assets/Images/serum1.png';
+import brightCreamImage from '../../assets/Images/brightening-cream.png';
 
 const ProductCard = ({ product, animationClass }) => {
   const [isFlipped, setIsFlipped] = useState(false);
@@ -18,9 +22,20 @@ const ProductCard = ({ product, animationClass }) => {
       <div className="card-face card-front">
         <div className="card-front-content">
           <div className="product-header">
-            {/* <div className="product-image">
-              <img src={serumImage} alt={product.title} />
-            </div> */}
+            <div className="product-image">
+              <img
+                src={
+                  {
+                    'facewash': facewashImage,
+                    'facewash1': facewash1Image,
+                    'serum': serumImage,
+                    'serum1': serum1Image,
+                    'brightening-cream': brightCreamImage
+                  }[product.id]
+                }
+                alt={product.title}
+              />
+            </div> 
             <h3>{product.title}</h3>
           </div>
           <div className="product-footer">
